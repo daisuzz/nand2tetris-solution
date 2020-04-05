a04e3a311fc6637d6240110f1fc36b89d29d40f0
daisuzz <daisaku.rugby@gmail.com>
2020-04-05 18:13:12 +0900

    スタック算術コマンドのみに対応したVM変換器を実装

---
+++ src/main/vmtranslator/Main.kt
@@ -0,0 +1,50 @@
+package vmtranslator
+
+import java.io.File
+
+fun main(args: Array<String>) {
+    val inputFilePath = args[0]
+    val inputFile = File(inputFilePath)
+
+    val outputFilePath = inputFilePath.substringBeforeLast(".") + ".asm"
+    val outputFile = File(outputFilePath)
+    if (outputFile.exists()) outputFile.delete()
+
+    val parser = Parser(inputFile)
+    val codeWriter = CodeWriter(outputFile)
+
+    loop@ while (parser.hasMoreCommands()) {
+        parser.advance()
+
+        when (parser.commandType()) {
+            CommandType.C_ARITHMETIC -> {
+                val command = parser.arg1()
+                codeWriter.writeArithmetic(command)
+            }
+            CommandType.C_PUSH, CommandType.C_POP -> {
+                val segment = parser.arg1()
+                val index = parser.arg2()
+                codeWriter.writePushPop("push", segment, index)
+            }
+            CommandType.C_LABEL -> {
+                TODO()
+            }
+            CommandType.C_GOTO -> {
+                TODO()
+            }
+            CommandType.C_IF -> {
+                TODO()
+            }
+            CommandType.C_FUNCTION -> {
+                TODO()
+            }
+            CommandType.C_CALL -> {
+                TODO()
+            }
+            CommandType.C_RETURN -> {
+                TODO()
+            }
+        }
+    }
+
+}
